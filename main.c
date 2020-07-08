830ca6cc28dcee39eec69d5e9806c5d7951f388e
Shiv <shivaanshseal@gmail.com>
2020-07-08 00:24:19 -0400

    Fixed iterative quicksort issue with odd value arrays

--- main.c
+++ main.c
@@ -77,7 +77,7 @@ void merge(int arr[], int lo, int mid, int hi){
     int i = lo, j = mid+1, k = lo;
     int buffer[99];
 
-    while(i <= mid && j <= hi){
+    while(i <= mid && j < hi){
         if(arr[i] < arr[j]){
             buffer[k++] = arr[i++];
         }else{
@@ -99,7 +99,7 @@ void merge(int arr[], int lo, int mid, int hi){
 void IMergeSort(int arr[], int size){
     //This loop runs log n times, list size always increases by 2x
     int p, l, h, mid, i;
-    for(p = 2; p <= size; p = p*2){
+    for(p = 1; p <= size; p = p*2){
         // breaks up main array into arrays of p elements each
         for(i = 0; i+p-1 <= size; i = i+p){
             //lower limit
@@ -120,33 +120,33 @@ int main()
 {
     printf("Array 1: \n");
     int array1[] = {5, 3, 4, 7, 1, 6, 2};
-    //displayArray(array1, 7);
+    displayArray(array1, 7);
     quickSort(array1, 0, 6);
-    //displayArray(array1, 7);
+    displayArray(array1, 7);
 
     printf("Array 2: \n");
     int array2[] = {1, 2, 3, 4, 5, 6, 7};
-    //displayArray(array2, 7);
+    displayArray(array2, 7);
     quickSort(array2, 0, 6);
-    //displayArray(array2, 7);
+    displayArray(array2, 7);
 
     printf("Array 3: \n");
     int array3[] = {7, 6, 5, 4, 3, 2, 1};
-    //displayArray(array3, 7);
+    displayArray(array3, 7);
     quickSort(array3, 0, 6);
-    //displayArray(array3, 7);
+    displayArray(array3, 7);
 
     printf("Array 4: \n");
     int array4[] = {5, 0, 77, 3, 1, -10, 49};
-    //displayArray(array4, 7);
+    displayArray(array4, 7);
     quickSort(array4, 0, 6);
-    //displayArray(array4, 7);
+    displayArray(array4, 7);
 
     printf("Array 1 bubblesort: \n");
     int array1a[] = {5, 3, 4, 7, 1, 6, 2};
-    //displayArray(array1a, 7);
+    displayArray(array1a, 7);
     bubblesort(array1a, 6);
-    //displayArray(array1a, 7);
+    displayArray(array1a, 7);
 
     printf("Array 1 iterative mergesort (odd values): \n");
     int array1b[] = {3, 5, 4, 7, 1, 6, 2};
@@ -158,6 +158,15 @@ int main()
     IMergeSort(array1c, 8);
     displayArray(array1c, 8);
 
+    printf("Array 2 iterative mergesort (even values): \n");
+    int array2b[] = {3, 5, -4, 8, 1, 6};
+    IMergeSort(array2b, 6);
+    displayArray(array2b, 6);
+
+    printf("Array 2 iterative mergesort (odd values): \n");
+    int array2c[] = {3, 5, -4, 7, 1, 6, -2, 8, 75};
+    IMergeSort(array2c, 9);
+    displayArray(array2c, 9);
 
     return 0;
 }
